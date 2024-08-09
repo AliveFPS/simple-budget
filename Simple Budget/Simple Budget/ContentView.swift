e240a4950522ee86720d3252cf9d899463aa00fe
Chris <cpena3@buffalo.edu>
2024-08-09 16:58:14 -0400

    Hello World

---
+++ Simple Budget/Simple Budget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Simple Budget
+//
+//  Created by Christopher Pena on 8/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simple Budget/Simple Budget/Simple_BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_BudgetApp.swift
+//  Simple Budget
+//
+//  Created by Christopher Pena on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Simple_BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
