5d089e6e28f1134e767f7009cb9286f455cbca73
Chris <cpena3@buffalo.edu>
2024-09-03 17:37:28 -0400

    Add Button Built, no functionality yet

---
+++ Simple Budget/Simple Budget/Views/AddCategoryView.swift
@@ -0,0 +1,29 @@
+//
+//  AddCategoryView.swift
+//  Simple Budget
+//
+//  Created by Christopher Pena on 9/3/24.
+//
+
+import SwiftUI
+
+struct AddCategoryView: View {
+    var body: some View {
+        Button(action: {
+            // Action to perform when the button is tapped
+            print("Add category button tapped")
+        }) {
+            Image(systemName: "plus")
+                .font(.system(size: 30, weight: .bold))
+                .frame(width: 200, height: 60)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .clipShape(Rectangle())
+                .cornerRadius(6)
+        }
+    }
+}
+
+#Preview {
+    AddCategoryView()
+}
